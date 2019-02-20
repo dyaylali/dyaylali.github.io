8b968ebbb3a41f5e583ec717ad90fe23232ca586
David, at Home <david.yaylali@gmail.com>
2019-02-19 17:04:03 -0700

    Update Date and new Meta tags

---
+++ updater.js
@@ -0,0 +1,6 @@
+init(); //on page load - show first slide, hidethe rest
+
+function init() {
+  document.getElementById("UPDATER").innerHTML = document.getElementById("UPDATER").innerHTML.replace('todaysDate', '2/19/19');
+
+}
