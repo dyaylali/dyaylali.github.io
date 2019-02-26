0f2eb0554726c1758d709a83222fbe4f6ca8b2af
David Yaylali <david.yaylali@gmail.com>
2019-02-25 17:03:24 -0700

    added PySwath

--- updater.js
+++ updater.js
@@ -1,6 +1,6 @@
 init(); //on page load - show first slide, hidethe rest
 
 function init() {
-  document.getElementById("UPDATER").innerHTML = document.getElementById("UPDATER").innerHTML.replace('todaysDate', '2/19/19');
+  document.getElementById("UPDATER").innerHTML = document.getElementById("UPDATER").innerHTML.replace('todaysDate', '2/25/19');
 
 }
