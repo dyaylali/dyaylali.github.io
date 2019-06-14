7cb26aaf67b4d6b1589f8110c2a72e10b4a911d8
David Yaylali <david.yaylali@gmail.com>
2019-06-13 22:51:01 -0700

    Long overdue update, just to home page for now

--- updater.js
+++ updater.js
@@ -1,6 +1,6 @@
 init(); //on page load - show first slide, hidethe rest
 
 function init() {
-  document.getElementById("UPDATER").innerHTML = document.getElementById("UPDATER").innerHTML.replace('todaysDate', '3/27/19');
+  document.getElementById("UPDATER").innerHTML = document.getElementById("UPDATER").innerHTML.replace('todaysDate', '6/13/19');
 
 }
